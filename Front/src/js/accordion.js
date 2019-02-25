f9ee38a33d611dc02c573c45c3cf2ca658181c09
Sarho Principato <sarho.principato@gmail.com>
2019-02-25 17:39:05 +0100

    JS update

--- Front/src/js/accordion.js
+++ Front/src/js/accordion.js
@@ -1,92 +1,93 @@
-function Accordion(options)
+class Accordion
 {
-	this.container = options.container;
-	this.mainTitle = options.mainTitle;
-	this.panels    = options.panels;
-}
-
-Accordion.prototype.start = function()
-{
-	const target = document.getElementById(this.container);
-
-	if(this.mainTitle)
+	constructor(options)
 	{
-		const main_title = document.createElement('h1');
-		main_title.className = 'accordion-main-title';
-		main_title.innerHTML = this.mainTitle;
-		target.appendChild(main_title);
+		this.container = options.container;
+		this.mainTitle = options.mainTitle;
+		this.panels    = options.panels;
 	}
 
-	if(this.panels.length > 0)
+	init()
 	{
-		var panels_wrapper = document.createElement('div');
-		panels_wrapper.className = 'panels-wrapper';
+		const target = document.getElementById(this.container);
 
-		// Creo il contenuto dei pannelli
-		this.panels.forEach(function(panel, index)
+		if (this.mainTitle)
 		{
-			let item       = document.createElement('div');
-			item.className = 'panel';
+			const main_title     = document.createElement('h1');
+			main_title.className = 'accordion-main-title';
+			main_title.innerHTML = this.mainTitle;
+			target.appendChild(main_title);
+		}
 
-			let expand       = document.createElement('i');
-			expand.className = 'material-icons';
-			expand.innerHTML = 'expand_more';
-			item.appendChild(expand);
+		if (this.panels.length > 0)
+		{
+			var panels_wrapper       = document.createElement('div');
+			panels_wrapper.className = 'panels-wrapper';
 
-			let title       = document.createElement('h2');
-			title.className = 'panel-title';
-			title.innerHTML = panel.title;
-			item.appendChild(title);
-
-			if(panel.subtitle)
+			// Creo il contenuto dei pannelli
+			this.panels.forEach(function (panel, index)
 			{
-				let subtitle       = document.createElement('h3');
-				subtitle.className = 'panel-subtitle';
-				subtitle.innerHTML = panel.subtitle;
-				item.appendChild(subtitle);
-			}
+				let item       = document.createElement('div');
+				item.className = 'panel';
 
-			let panel_content           = document.createElement('div');
+				let expand       = document.createElement('i');
+				expand.className = 'material-icons';
+				expand.innerHTML = 'expand_more';
+				item.appendChild(expand);
+
+				let title       = document.createElement('h2');
+				title.className = 'panel-title';
+				title.innerHTML = panel.title;
+				item.appendChild(title);
+
+				if (panel.subtitle)
+				{
+					let subtitle       = document.createElement('h3');
+					subtitle.className = 'panel-subtitle';
+					subtitle.innerHTML = panel.subtitle;
+					item.appendChild(subtitle);
+				}
+
+				let panel_content       = document.createElement('div');
 				panel_content.className = 'panel-content';
 
-			let doc            = new DOMParser(),
-				nodes          = doc.parseFromString(panel.content, 'text/html'),
-				filtered_nodes = nodes.body.childNodes;
+				let doc = new DOMParser(), nodes = doc.parseFromString(panel.content, 'text/html'), filtered_nodes = nodes.body.childNodes;
 
-			// Converto i nodi in array
-			let content = Array.from(filtered_nodes);
+				// Converto i nodi in array
+				let content = Array.from(filtered_nodes);
 
-			content.forEach(function(item)
-			{
-				panel_content.appendChild(item);
-			});
-
-			item.appendChild(panel_content);
-
-			// Gestione azione dei pannelli dell'accordion
-			item.addEventListener("click", function()
-			{
-				let elements = panels_wrapper.children;
-
-				for(let i = 0; i < elements.length; i++)
+				content.forEach(function (item)
 				{
-					if(this === elements[i])
-					{
-						this.classList.toggle("active")
-					}
+					panel_content.appendChild(item);
+				});
 
-					else
+				item.appendChild(panel_content);
+
+				// Gestione azione dei pannelli dell'accordion
+				item.addEventListener("click", function ()
+				{
+					let elements = panels_wrapper.children;
+
+					for (let i = 0; i < elements.length; i++)
 					{
-						elements[i].classList.remove('active');
-						elements[i].getElementsByTagName("i")[0].innerHTML = "expand_more";
+						if (this === elements[i])
+						{
+							this.classList.toggle("active");
+						}
+
+						else
+						{
+							elements[i].classList.remove('active');
+							elements[i].getElementsByTagName("i")[0].innerHTML = "expand_more";
+						}
 					}
-				}
+				});
+
+				// Inserisco il pannello (item) nel div contenitore
+				panels_wrapper.appendChild(item);
 			});
+		}
 
-			// Inserisco il pannello (item) nel div contenitore
-			panels_wrapper.appendChild(item);
-		 });
+		target.appendChild(panels_wrapper);
 	}
-
-	target.appendChild(panels_wrapper);
-};
+}
